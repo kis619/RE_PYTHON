5fb7c96636687e85cfe5cd9c1598c765ac87eb47
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-05-04 08:59:37 +0200

    first two modules

---
+++ ex00/Hello.py
@@ -0,0 +1,27 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+#updating list
+ft_list[1] = "World!"
+
+#updating tuple
+# - convert tuple to list
+temp = list(ft_tuple)
+# - change list item
+temp[1] = "Germany"
+# - convert list back to tuple
+ft_tuple = tuple(temp)
+
+#updating set
+ft_set.remove("tutu!")
+ft_set.add("Wolfsburg")
+
+#updating dictionary
+ft_dict["Hello"] = "42Wolfsburg!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
---
+++ ex01/format_ft_time.py
@@ -0,0 +1,17 @@
+import time
+
+#time since UNIX epoch (01.01.1970)
+epoch_time: float = time.time()
+
+# format time {:,.4f}/{:,.2e}
+# The comma (,) character serves as the thousands separator.
+# The . character indicates the start of the precision specifier.
+# The 4 specifies the number of decimal places.
+# The f indicates that the number should be formatted as a floating-point value.
+# The e indicates that the number should be formatted as a floating-point value.
+# Without f or e we get the scientific notation
+
+#print in the desired format
+print(f"Seconds since January 1, 1970: {'{:,.4f}'.format(epoch_time)} or {'{:,.2e}'.format(epoch_time)} in scientific notation")
+print(time.strftime("%B %d %Y"))
+
