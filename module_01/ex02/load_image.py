dc084ae28b779db98b4bd2f0eb97ded8daebc5dc
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-05-10 07:35:43 +0200

    ex02 + tests

---
+++ module_01/ex02/load_image.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import numpy as np
+
+
+def ft_load(path: str) -> np.ndarray | None:
+    if not isinstance(path, str):
+        print("Path must be a string")
+        return None
+
+    try:
+        img = Image.open(path)
+    except FileNotFoundError:
+        print(f"File at path {path} not found")
+        return None
+
+    format = img.format
+    print("Image format:", format)
+    format_to_pixel_array_funcitons = {
+        "PNG": lambda img: np.array(img.convert('RGBA')),
+        "JPEG": lambda img: np.array(img),
+    }
+    if format not in format_to_pixel_array_funcitons:
+        print("Unsupported file format")
+        img.close()
+        return None
+    pixel_array = format_to_pixel_array_funcitons[format](img)
+    img.close()
+    print("The shape of the image is:", pixel_array.shape)
+    return pixel_array
+
+
+print(ft_load(2))
+print(ft_load("noob-level.png"))
+print(ft_load("landscape.jpg"))
+
---
+++ module_01/tests/tests02.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("..")
+
+from ex02.load_image import ft_load
+from unittest import TestCase, main
+from unittest.mock import patch
+from io import StringIO
+
+def my_function():
+    print("Hello, world!")
+
+class TestFtLoad(TestCase):
+    
+    # The @patch('sys.stdout', new_callable=StringIO) decorator
+    # is used to replace the standard output with a StringIO object,
+    # which allows us to capture the output of the print() function
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_ft_load_file_not_found(self, mock_stdout):
+        self.assertEqual(ft_load("not_found.png"), None)
+        self.assertEqual(mock_stdout.getvalue(), "File at path not_found.png not found\n")
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_ft_load_unsupported_file_format(self, mock_stdout):
+        self.assertEqual(ft_load("module_01/normal-level.webp"), None)
+        self.assertEqual(mock_stdout.getvalue(), "Image format: WEBP\nUnsupported file format\n")
+    
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_ft_load_argument_not_string(self, mock_stdout):
+        self.assertEqual(ft_load(2), None)
+        self.assertEqual(mock_stdout.getvalue(), "Path must be a string\n")
+
+    def test_ft_load_shape(self):
+        self.assertEqual(ft_load("module_01/landscape.jpg").shape, (257, 450, 3))
+        
+if __name__ == "__main__":
+    main()
