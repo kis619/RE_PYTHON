d857c8bf01e9c14fb9364e640469a4e77ba5cc98
kis619 <kis619@abv.bg>
2023-05-17 13:37:53 +0200

    calculator done

---
+++ module_03/ex03/ft_calculator.py
@@ -0,0 +1,27 @@
+class calculator:
+
+    def __init__(self, data):
+        """Constructor
+        Atributtes: data (list)
+        """
+        self.data = data
+
+    def __add__(self, other) -> None:
+        """Addition of a number to each element of the list"""
+        self.data = [x + other for x in self.data]
+        print(self.data)
+
+    def __sub__(self, other) -> None:
+        """Subtraction of a number to each element of the list"""
+        self.data = [x - other for x in self.data]
+        print(self.data)
+
+    def __mul__(self, other) -> None:
+        """Multiplication of a number to each element of the list"""
+        self.data = [x * other for x in self.data]
+        print(self.data)
+
+    def __truediv__(self, other) -> None:
+        """Division of a number to each element of the list"""
+        self.data = [x / other for x in self.data]
+        print(self.data)
