95dea1191ceb573d4281295161f5991dbf54b7ea
kis619 <kis619@abv.bg>
2023-05-18 17:33:38 +0200

    experiment stage for ex03

---
+++ module_04/ex03/new_student.py
@@ -0,0 +1,16 @@
+# import random
+# import string
+# from dataclasses import dataclass, field
+
+# def generate_id() -> str:
+#     return "".join(random.choices(string.ascii_lowercase, k = 15))
+
+# @dataclass
+# class Student:
+#     def self.__ini
+    
+# def main():
+#     ...
+
+# if __name__ == "__main__":
+#     main()
